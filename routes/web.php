63ec92d35eaa18d6c7ffe7660badcfc1b754d516
Kishan Bhatt <iamkishanbhatt@gmail.com>
2021-11-01 10:36:13 +0530

    inertia package added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Inertia\Inertia;
+
+Route::get('home', function(){
+  return Inertia::render('Home');
+});
